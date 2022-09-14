d8799824ef187aecbff64f4fc34ac7be77282201
jmg <macgreene14@gmail.com>
2022-09-14 16:47:21 -0400

    App working

--- index.js
+++ index.js
@@ -12,56 +12,140 @@ const generateHTML = require("./src/generateHTML.js");
 const employees = [];
 
 // prompt users for inputs for Manager class
-inquirer
-  .prompt([
-    {
-      type: "input",
-      name: "managerName",
-      message: "Add Manager Name",
-    },
-    {
-      type: "input",
-      name: "managerId",
-      message: "Add Manager ID",
-    },
-    {
-      type: "input",
-      name: "managerEmail",
-      message: "Add Manager Email",
-    },
-    {
-      type: "input",
-      name: "managerOfficeNum",
-      message: "Add Manager Office Number",
-    },
-  ])
-  .then((a) => {
-    const manager = new Manager(
-      a.managerName,
-      a.managerId,
-      a.managerEmail,
-      a.managerOfficeNum
-    );
-    employees.push(manager);
-  });
 
-// promp users for inputs for other classes or to generate HTML
-inquirer
-  .prompt([
-    {
-      type: "list",
-      name: "action",
-      message: "Select an option",
-      choice: ["Add Engineer", "Add Intern", "Generate HTML"],
-    },
-  ])
-  .then((a) => {
-    if (a.action === "Generate HTML") {
-      // run function
-      generateHTML();
-    } else if (a.action === "Add Engineer") {
-      //run prompt
-    } else if (a.action === "Add Engineer") {
-      //run prompt
-    }
-  });
+function main() {
+  // add manager
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "managerName",
+        message: "Add Manager Name",
+      },
+      {
+        type: "input",
+        name: "managerId",
+        message: "Add Manager ID",
+      },
+      {
+        type: "input",
+        name: "managerEmail",
+        message: "Add Manager Email",
+      },
+      {
+        type: "input",
+        name: "managerOfficeNum",
+        message: "Add Manager Office Number",
+      },
+    ])
+    .then((a) => {
+      const manager = new Manager(
+        a.managerName,
+        a.managerId,
+        a.managerEmail,
+        a.managerOfficeNum
+      );
+      employees.push(manager);
+      console.log("Manager added to roster!");
+    })
+    .then(() => addEmployee());
+}
+
+function addEmployee() {
+  // promp users for inputs for other classes or to generate HTML
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "action",
+        message: "Select an option",
+        choices: ["Add Engineer", "Add Intern", "Generate HTML"],
+      },
+    ])
+    .then((a) => {
+      if (a.action === "Generate HTML") {
+        // run function
+        generateHTML(employees);
+        return; // breaks function
+      } else if (a.action === "Add Engineer") {
+        //run prompt
+        inquirer
+          .prompt([
+            {
+              type: "input",
+              name: "engineerName",
+              message: "Add Engineer Name",
+            },
+            {
+              type: "input",
+              name: "engineerId",
+              message: "Add Engineer ID",
+            },
+            {
+              type: "input",
+              name: "engineerEmail",
+              message: "Add Engineer Email",
+            },
+            {
+              type: "input",
+              name: "engineerGithub",
+              message: "Add Github URL",
+            },
+          ])
+          .then((a) => {
+            const engineer = new Engineer(
+              a.engineerName,
+              a.engineerId,
+              a.engineerEmail,
+              a.engineerGithub
+            );
+            employees.push(engineer);
+          })
+          .then(() => {
+            console.log("Engineer added to roster!");
+            addEmployee(); // restart function
+          });
+      } else if (a.action === "Add Intern") {
+        //run prompt
+        inquirer
+          .prompt([
+            {
+              type: "input",
+              name: "internName",
+              message: "Add Intern Name",
+            },
+            {
+              type: "input",
+              name: "internId",
+              message: "Add Intern ID",
+            },
+            {
+              type: "input",
+              name: "internEmail",
+              message: "Add Intern Email",
+            },
+            {
+              type: "input",
+              name: "internSchool",
+              message: "Add School",
+            },
+          ])
+          .then((a) => {
+            const intern = new Intern(
+              a.internName,
+              a.internId,
+              a.internEmail,
+              a.internSchool
+            );
+            employees.push(intern);
+          })
+          .then(() => {
+            console.log("Intern added to roster!");
+            addEmployee(); // restart function
+          });
+      }
+    });
+}
+
+// run main function
+main();
--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,5 +1,9 @@
 fs = require("fs");
 
-function generateHTML() {
+function generateHTML(employees) {
   // accept classes, generate cards, insert into template, save local
+  console.log(employees);
+  console.log("HTML Generated!");
 }
+
+module.exports = generateHTML;
